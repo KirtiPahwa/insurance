0f7e8516af599afafd679f4ca6798e3af2ccda4d
harsh <apkharsh@gmail.com>
2023-10-23 01:14:01 +0530

    cart is styled

--- Frontend1/src/Component/Cart/Cart.js
+++ Frontend1/src/Component/Cart/Cart.js
@@ -1,27 +1,123 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+// import "./Cart.css";
 
 const Cart = () => {
-    const handleClaim=()=>
-    {
+    const handleClaim = () => {
         window.location.reload(false);
-        localStorage.setItem("Claimed","true");
+        localStorage.setItem("Claimed", "true");
         localStorage.removeItem("health");
         localStorage.removeItem("home");
         localStorage.removeItem("vehicle");
+    };
+    return (
+        <>
+            <div className="container" style={{display: "flex", flexDirection: "column"}}>
+                <h1
+                    style={{
+                        fontSize: "40px",
+                        color: "black",
+                        fontWeight: "800",
+                        textAlign: "center",
+                    }}
+                >
+                    Your cart
+                </h1>
+                {localStorage.getItem("home") == null ? (
+                    <div className="col-lg-12">
+                        <div
+                            className="feature wow fadeInUp"
+                            data-wow-delay=".4s"
+                        >
+                            <div className="feature-title">
+                                <i className="icon-bar-chart-up"></i>
+                                <h2 className="title">Customer Services</h2>
+                                <small className="subtitle">
+                                    Nulla eros odio dolor
+                                </small>
+                            </div>
+                            <div className="feature-summary">
+                                <p>
+                                    Chocolate caramels unerdwear.com lemon
+                                    drops. Powder chupa chups pastry macaroon
+                                    wafer chocolate cake sweet roll croissant
+                                    jelly
+                                </p>
+                            </div>
+                            {/* <Link to="/policy/vehicle" className="button">
+                                    More info
+                                </Link> */}
+                        </div>
+                    </div>
+                ) : (
+                    <h2>Empty</h2>
+                )}
 
+                {localStorage.getItem("health") == null ? (
+                    <div className="col-lg-12">
+                        <div
+                            className="feature wow fadeInUp"
+                            data-wow-delay=".4s"
+                        >
+                            <div className="feature-title">
+                                <i className="icon-bar-chart-up"></i>
+                                <h2 className="title">Customer Services</h2>
+                                <small className="subtitle">
+                                    Nulla eros odio dolor
+                                </small>
+                            </div>
+                            <div className="feature-summary">
+                                <p>
+                                    Chocolate caramels unerdwear.com lemon
+                                    drops. Powder chupa chups pastry macaroon
+                                    wafer chocolate cake sweet roll croissant
+                                    jelly
+                                </p>
+                            </div>
+                            {/* <Link to="/policy/vehicle" className="button">
+                                More info
+                            </Link> */}
+                        </div>
+                    </div>
+                ) : (
+                    <h2>Empty</h2>
+                )}
+                {localStorage.getItem("vehicle") == null ? (
+                    <div className="col-lg-12">
+                        <div
+                            className="feature wow fadeInUp"
+                            data-wow-delay=".4s"
+                        >
+                            <div className="feature-title">
+                                <i className="icon-bar-chart-up"></i>
+                                <h2 className="title">Customer Services</h2>
+                                <small className="subtitle">
+                                    Nulla eros odio dolor
+                                </small>
+                            </div>
+                            <div className="feature-summary">
+                                <p>
+                                    Chocolate caramels unerdwear.com lemon
+                                    drops. Powder chupa chups pastry macaroon
+                                    wafer chocolate cake sweet roll croissant
+                                    jelly
+                                </p>
+                            </div>
+                            {/* <Link to="/policy/vehicle" className="button">
+                                More info
+                            </Link> */}
+                        </div>
+                    </div>
+                ) : (
+                    <h2>Empty</h2>
+                )}
 
-    }
-  return (
-    <>
-    <h1>Your cart</h1>
-        {localStorage.getItem("home")!=null?<h1>home</h1>:<h1>Empty</h1>}
-        
-        {localStorage.getItem("health")!=null?<h1>health</h1>:<h1>Empty</h1>}
-        {localStorage.getItem("vehicle")!=null?<h1>vehicle</h1>:<h1>Empty</h1>}
-<button type='submit' onClick={handleClaim}>Claim</button>
-  
-    </>
-  )
-}
+                <button type="submit" onClick={handleClaim}>
+                    Claim
+                </button>
+            </div>
+        </>
+    );
+};
 
-export default Cart
+export default Cart;
