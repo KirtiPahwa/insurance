349ab4c39e233bce78a5ed25697f1e840e8e2d81
Harsh kumar <apkharsh@gmail.com>
2023-10-23 00:42:42 +0530

    enjoy

--- Frontend1/src/App.js
+++ Frontend1/src/App.js
@@ -1,22 +1,22 @@
-import Register from './Component/Register/Register';
- import Login from './Component/Login/Login';
+import Register from "./Component/Register/Register";
+import Login from "./Component/Login/Login";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from './Component/Home/Home';
-import { Policy } from './Component/Policy/Policy';
+import Home from "./Component/Home/Home";
+import { Policy } from "./Component/Policy/Policy";
 
 function App() {
-  return (
-    <div className="App">
-<BrowserRouter>
-        <Routes>
-          <Route path="/login" element={<Login />} exact />
-          <Route path="/register" element={<Register />} exact />
-          <Route path="/" element={<Home />} exact />
-          <Route path="/policy/:type" element={<Policy />} exact />
-        </Routes>
-      </BrowserRouter>
-    </div>
-  );
+    return (
+        <div className="App">
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/login" element={<Login />} exact />
+                    <Route path="/register" element={<Register />} exact />
+                    <Route path="/" element={<Home />} exact />
+                    <Route path="/policy/:type" element={<Policy />} exact />
+                </Routes>
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
