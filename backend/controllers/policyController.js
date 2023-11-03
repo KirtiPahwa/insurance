72cfe5e09fdaedd7cb77aa9e434a65305e5363c4
pahwakirti09942@gmail.com <pahwakirti09942@gmail.com>
2023-11-03 18:59:11 +0530

    frontend ready

--- backend/controllers/policyController.js
+++ backend/controllers/policyController.js
@@ -1,10 +1,20 @@
 const policy = require("../models/policy");
 
-const createPolicy = async(req, res) => {
+const createPolicy = async (req, res) => {
     // policy data in body
     const newPolicy = new policy(req.body);
     // save to db
     await newPolicy.save();
-}
+};
 
-module.exports = { createPolicy };
+const allPolicies = async (req, res) => {
+    try {
+        const policies = await policy.find();
+        console.log(policies);
+        res.status(200).json( policies );
+    } catch (err) {
+        res.status(500).json({ error: "Error fetching policies" });
+    }
+};
+
+module.exports = { createPolicy, allPolicies };
